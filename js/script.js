57f8e3385315185b41cb4d30ecce9c3d02a44730
ghelgameshra <ghelgameshra3347@gmail.com>
2022-10-27 04:08:40 +0700

    finish navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-// alert("Hello");
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function(){
+    nav.classList.toggle('slide');
+});
